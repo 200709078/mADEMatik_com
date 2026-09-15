c72c4f3ced66bfc10bd057584f261d7c412a7073
mADEMatik Migration <admin@madematik.com>
2026-09-15 19:57:24 +0300

    Ust bar panel mavisi; kullanici/cikis sol menu stiliyle ayni

--- resources/views/back/layouts/menu.blade.php
+++ resources/views/back/layouts/menu.blade.php
@@ -67,7 +67,7 @@
     </ul>
     <div id="content-wrapper" class="d-flex flex-column">
         <div id="content">
-            <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+            <nav class="navbar navbar-expand navbar-dark topbar mb-4 static-top shadow">
                 <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
                     <i class="fa fa-bars"></i>
                 </button>
@@ -76,7 +76,7 @@
                     <li class="nav-item dropdown no-arrow">
                         <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <span class="mr-2 d-none d-lg-inline text-gray-600 small">{{Auth::user()->name}}</span>
+                            <span class="mr-2 d-none d-lg-inline small topbar-username">{{Auth::user()->name}}</span>
                         </a>
                         <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
                              aria-labelledby="userDropdown">
