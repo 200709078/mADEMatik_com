7e3aeb3ff41ed3199065a2b14f1dff61689fb3f4
mADEMatik Migration <admin@madematik.com>
2026-09-15 22:44:56 +0300

    DataTables tum tablolarda Turkce (info, onceki/sonraki)

--- public/js/datatables-demo.js
+++ public/js/datatables-demo.js
@@ -1,4 +1,26 @@
 // Call the dataTables jQuery plugin
 $(document).ready(function() {
-  $('#dataTable').DataTable();
+  $('#dataTable').DataTable({
+    language: {
+      emptyTable: 'Tabloda kayıt bulunamadı',
+      info: '_TOTAL_ kayıttan _START_ - _END_ arası gösteriliyor',
+      infoEmpty: '0 kayıttan 0 - 0 arası gösteriliyor',
+      infoFiltered: '(_MAX_ kayıt arasından filtrelendi)',
+      lengthMenu: 'Sayfada _MENU_ kayıt göster',
+      loadingRecords: 'Yükleniyor...',
+      processing: 'İşleniyor...',
+      search: 'Ara:',
+      zeroRecords: 'Eşleşen kayıt bulunamadı',
+      paginate: {
+        first: 'İlk',
+        last: 'Son',
+        next: 'Sonraki',
+        previous: 'Önceki'
+      },
+      aria: {
+        sortAscending: ': artan sıralama için tıklayın',
+        sortDescending: ': azalan sıralama için tıklayın'
+      }
+    }
+  });
 });
